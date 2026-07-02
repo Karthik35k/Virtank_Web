a7a67cc24c9d97a5bfae59d5612778e9d2d06f74
Karthik35k <karthik3vk@gmail.com>
2026-07-02 17:22:12 +0530

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+import cors from "cors";
+
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+
+app.use(cors());
+
+app.use(express.json());
+
+// Routes
+
+app.use(
+  "/api/auth",
+  authRoutes
+);
+
+// Default Route
+
+app.get("/", (req, res) => {
+
+  res.send(
+    "Social Media API Running"
+  );
+
+});
+
+export default app;
---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+
+import app from "./app.js";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+// Connect Database
+
+connectDB();
+
+// Start Server
+
+const PORT =
+  process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+  console.log(
+    `Server Running on Port ${PORT}`
+  );
+
+});
