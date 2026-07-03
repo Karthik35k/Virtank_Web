7a816ebebc4847d2a08d19d505455b3f5988e030
Karthik35k <karthik3vk@gmail.com>
2026-07-03 14:40:51 +0530

    Fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,6 @@
 import express from "express";
-
 import cors from "cors";
-
 import dotenv from "dotenv";
-
 import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
@@ -12,25 +9,25 @@ const app = express();
 
 // Middleware
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://virtank-web.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(express.json());
 
 // Routes
 
-app.use(
-  "/api/auth",
-  authRoutes
-);
+app.use("/api/auth", authRoutes);
 
 // Default Route
 
 app.get("/", (req, res) => {
-
-  res.send(
-    "Social Media API Running"
-  );
-
+  res.send("Social Media API Running");
 });
 
 export default app;
