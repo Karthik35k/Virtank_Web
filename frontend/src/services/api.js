ff6a2d0fa56cab99c14ec91b95f92138cb729ad6
Karthik35k <karthik3vk@gmail.com>
2026-07-03 14:49:17 +0530

    Fixed Error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://virtank-backend.onrender.com",
+  baseURL: "https://virtank-backend.onrender.com/api"
 });
 
 export default API;
